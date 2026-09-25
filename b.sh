1671b376395839c8a771fe2f61a886943fbb631c
karan <xkaranx5@gmail.com>
2026-09-25 05:35:06 +0530

    refactor: update script structure by defining variables for project paths and improving readability

--- b.sh
+++ b.sh
@@ -2,11 +2,15 @@ set -e
 
 # node test.ts
 
-cd /home/karan/Desktop/aigalaxy/server
+PROJECT_DIR=/home/karan/Desktop/aigalaxy/server
+VM_NAME=azvm
+VM_HOME_PATH=/home/azureuser
+
+cd $PROJECT_DIR
 
 source /home/karan/Desktop/ci-cd/.env
 
 # npm run test
 npm run build
-scp -r ./dist pcvm:/home/azureuser/aigalaxy/server
-ssh pcvm "/home/azureuser/.local/share/pnpm/bin/pm2 reload aidebate"
+scp -r $PROJECT_DIR/dist $VM_NAME:$VM_HOME_PATH/aigalaxy/server
+ssh $VM_NAME "$VM_HOME_PATH/.local/share/pnpm/bin/pm2 reload aidebate"
--- f.sh
+++ f.sh
@@ -2,11 +2,15 @@ set -e
 
 # node test.ts
 
-cd /home/karan/Desktop/aigalaxy/client
+PROJECT_DIR=/home/karan/Desktop/aigalaxy/client
+BUCKET_NAME=aidebate-site
+
+cd $PROJECT_DIR
 
 source /home/karan/Desktop/ci-cd/.env
 
-# npm run test
+# npm ci
 npm run build
-aws s3 cp ./dist s3://aidebate-site/ --recursive
+# aws s3 cp ./dist s3://aidebate-site/ --recursive
+aws s3 sync $PROJECT_DIR/dist/ s3://$BUCKET_NAME --delete
 aws cloudfront create-invalidation  --distribution-id $CF_DIST_ID  --paths "/index.html"
